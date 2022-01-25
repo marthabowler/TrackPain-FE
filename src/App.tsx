dd3ba469bcb3d40d0d9e61b344a7c39e26335723
Martha Bowler <mbowler133@gmail.com>
2022-01-25 12:56:45 +0000

    Add painkiller to graph

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { PainType } from "./utils/Types/PainType";
 import { config } from "dotenv";
-import { Line } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
+import Graph from "./Components/Graph";
 
 config();
 
@@ -41,22 +21,11 @@ function App(): JSX.Element {
     getPainData();
   }, []);
 
-  const chartData = {
-    labels: painData.map((dataPoint: { time: string }) => dataPoint.time),
-    datasets: [
-      {
-        label: "Pain seriousness",
-        data: painData.map(
-          (dataPoint: { seriousness: number }) => dataPoint.seriousness
-        ),
-        fill: true,
-        backgroundColor: "rgba(75,192,192,0.2)",
-        borderColor: "rgba(75,192,192,1)",
-      },
-    ],
-  };
-
-  return <>{chartData ? <Line data={chartData} /> : <p>Data Loading</p>}</>;
+  return (
+    <>
+      <Graph painData={painData} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Graph.tsx
@@ -0,0 +1,62 @@
+import { PainType } from "../utils/Types/PainType";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface GraphProps {
+  painData: PainType[];
+}
+
+export default function Graph(props: GraphProps): JSX.Element {
+  const chartData = {
+    labels: props.painData.map((dataPoint: { time: string }) => dataPoint.time),
+    datasets: [
+      {
+        label: "Pain seriousness",
+        data: props.painData.map(
+          (dataPoint: { seriousness: number }) => dataPoint.seriousness
+        ),
+        backgroundColor: "red",
+        borderColor: "green",
+      },
+      {
+        label: "Painkiller",
+        data: props.painData.map(
+          (dataPoint: { painkiller_id: number }) => dataPoint.painkiller_id
+        ),
+
+        backgroundColor: "red",
+      },
+    ],
+  };
+
+  return (
+    <>
+      <div>
+        {chartData ? (
+          <Line className="chart" data={chartData} />
+        ) : (
+          <p>Data Loading</p>
+        )}
+      </div>
+    </>
+  );
+}
