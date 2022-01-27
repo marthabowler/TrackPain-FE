4cdb0380a84eda19b1fe3400fd16442579b46fcf
Martha Bowler <mbowler133@gmail.com>
2022-01-27 16:31:47 +0000

    Add multiple graphs for conditions

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { PainType } from "./utils/Types/PainType";
+import { UserType } from "./utils/Types/UserType";
 import { ConditionsType } from "./utils/Types/ConditionsType";
 import { config } from "dotenv";
 import Graph from "./Components/Graph";
@@ -10,6 +11,7 @@ import { InputData } from "./Components/InputData";
 import NavBar from "./Components/NavBar";
 import "./App.css";
 import { PainkillerType } from "./utils/Types/PainkillerType";
+import { UserConditionsType } from "./utils/Types/UserConditionType";
 
 config();
 
@@ -19,6 +21,14 @@ function App(): JSX.Element {
   const [painData, setPainData] = useState<PainType[]>([]);
   const [painkillerData, setPainkillerData] = useState<PainkillerType[]>([]);
   const [conditionsData, setConditionsData] = useState<ConditionsType[]>([]);
+  const [signedInUser, setSignedUser] = useState<UserType>({
+    username: "guest",
+    user_id: 1,
+    condition_id: 1,
+  });
+  const [signedInUserConditions, setSignedUserConditions] = useState<
+    UserConditionsType[]
+  >([]);
 
   const getAllData = useCallback(async () => {
     const painResponse = await axios.get(`${apiBaseURL}pain`);
@@ -27,7 +37,23 @@ function App(): JSX.Element {
     setPainkillerData(painkillerResponse.data.data);
     const conditionsResponse = await axios.get(`${apiBaseURL}conditions`);
     setConditionsData(conditionsResponse.data.data);
-  }, [setConditionsData, setPainkillerData, setPainData]);
+    const signedInUserResponse = await axios.get(
+      `${apiBaseURL}user/${signedInUser.user_id}`
+    );
+    setSignedUserConditions(signedInUserResponse.data.data);
+    setSignedUser({
+      username: "guest",
+      user_id: 1,
+      condition_id: 1,
+    });
+  }, [
+    setConditionsData,
+    setPainkillerData,
+    setPainData,
+    setSignedUserConditions,
+    signedInUser.user_id,
+    setSignedUser,
+  ]);
 
   useEffect(() => {
     console.log("getPainData called");
@@ -41,7 +67,15 @@ function App(): JSX.Element {
           <NavBar />
           <Routes>
             {/* different pages */}
-            <Route path="/" element={<Graph painData={painData} />} />
+            <Route
+              path="/"
+              element={
+                <Graph
+                  signedInUserConditions={signedInUserConditions}
+                  painData={painData}
+                />
+              }
+            />
             <Route path="/data" element={<InterestingData />} />
             <Route
               path="/input-data"
@@ -49,6 +83,8 @@ function App(): JSX.Element {
                 <InputData
                   painkillerData={painkillerData}
                   conditionsData={conditionsData}
+                  setPainkillerData={setPainkillerData}
+                  setConditionsData={setConditionsData}
                 />
               }
             />
---
+++ src/utils/Types/UserType.tsx
@@ -0,0 +1,5 @@
+export interface UserType {
+  username: string;
+  user_id: number;
+  condition_id: number;
+}
