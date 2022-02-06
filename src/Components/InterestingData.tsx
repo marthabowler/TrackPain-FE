62d4a7d980a6fc3fed1d8d94cee9171a065c980f
Martha Bowler <mbowler133@gmail.com>
2022-02-06 17:10:33 +0000

    Added function to create the data for interesting charts

--- src/Components/InterestingData.tsx
+++ src/Components/InterestingData.tsx
@@ -1,4 +1,3 @@
-import { PainkillerCorrelationType } from "../utils/Types/PainkillerCorrelationType";
 import axios from "axios";
 
 import {
@@ -12,8 +11,10 @@ import {
   Legend,
   BarElement,
 } from "chart.js";
-import { useEffect, useState } from "react";
-import { Bar } from "react-chartjs-2";
+import { useCallback, useEffect, useState } from "react";
+// import { Bar } from "react-chartjs-2";
+import { hasntWorkedType } from "../utils/Types/HasntWorkedType";
+import { hasWorkedType } from "../utils/Types/hasWorkedType";
 
 ChartJS.register(
   CategoryScale,
@@ -29,56 +30,120 @@ ChartJS.register(
 const apiBaseURL = process.env.REACT_APP_API_BASE;
 
 export default function InterestingData(): JSX.Element {
-  const [painkillerCorrelation, setPainkillerCorrelation] = useState<
-    PainkillerCorrelationType[]
-  >([]);
+  const [hasWorked, setHasWorked] = useState<hasWorkedType[]>([]);
+  const [hasntWorked, setHasntWorked] = useState<hasntWorkedType[]>([]);
+  const [interestingData, setInterestingData] = useState<dataType[]>([]);
 
-  async function getPainkillerCorrelationData() {
-    const response = await axios.get(`${apiBaseURL}correlations/painkillers`);
-    setPainkillerCorrelation(response.data.data);
-  }
+  const getHasWorkedandHasntWorked = useCallback(async () => {
+    const workedResponse = await axios.get(
+      `${apiBaseURL}correlations/hasworked`
+    );
+    setHasWorked(workedResponse.data.data);
+
+    const notWorkedResponse = await axios.get(
+      `${apiBaseURL}correlations/notworked`
+    );
+    setHasntWorked(notWorkedResponse.data.data);
+  }, [setHasWorked, setHasntWorked]);
 
   useEffect(() => {
-    getPainkillerCorrelationData();
-  }, []);
+    getHasWorkedandHasntWorked();
+  }, [getHasWorkedandHasntWorked]);
 
-  function createDiseaseArray(
-    painkillerCorrelation: PainkillerCorrelationType[]
-  ) {
-    const diseaseArray: number[] = [];
-    for (const obj of painkillerCorrelation) {
-      for (const [key, value] of Object.entries(obj)) {
-        if (key === "condition_id" && !diseaseArray.includes(value)) {
-          diseaseArray.push(value);
+  interface dataType {
+    condition_name: string;
+    painkiller_name: string;
+    has_worked: number;
+  }
+
+  const createGraphData = useCallback(
+    (hasWorked, hasntWorked) => {
+      const newHasWorked = hasWorked;
+      const newHasntWorked = hasntWorked;
+      for (let i = 0; i < newHasWorked.length; i++) {
+        for (let u = 0; u < newHasntWorked.length; u++) {
+          if (
+            hasWorked[i].condition_name === hasntWorked[u].condition_name &&
+            hasWorked[i].painkiller_name === hasntWorked[u].painkiller_name
+          ) {
+            interestingData.push({
+              condition_name: hasWorked[i].condition_name,
+              painkiller_name: hasWorked[i].painkiller_name,
+              has_worked:
+                (parseInt(hasWorked[i].has_worked) /
+                  (parseInt(hasntWorked[u].not_worked) +
+                    parseInt(hasWorked[i].has_worked))) *
+                100,
+            });
+            newHasWorked.splice(i, 1);
+
+            newHasntWorked.splice(u, 1);
+          }
         }
       }
-    }
-    return diseaseArray;
-  }
+      newHasWorked.forEach((element: hasWorkedType) =>
+        interestingData.push({
+          condition_name: element.condition_name,
+          painkiller_name: element.painkiller_name,
+          has_worked: 100,
+        })
+      );
 
-  function createChartData(painkillerCorrelation: PainkillerCorrelationType[]) {
-    const datasets = [];
+      newHasntWorked.forEach((element: hasntWorkedType) =>
+        interestingData.push({
+          condition_name: element.condition_name,
+          painkiller_name: element.painkiller_name,
+          has_worked: 0,
+        })
+      );
+      return interestingData;
+    },
+    [interestingData]
+  );
 
-    for (const disease of createDiseaseArray(painkillerCorrelation)) {
-      const oneLine = {
-        label: findConditionName(painkillerCorrelation, disease),
-        data: filterDataByCondition(painkillerCorrelation, disease).map(
-          (dataPoint: { avg: string }) => parseInt(dataPoint.avg)
-        ),
-        backgroundColor: "#FF0080",
-        borderColor: "#FF8C00",
-      };
-      datasets.push(oneLine);
-    }
+  useEffect(() => {
+    hasWorked.length > 0 &&
+      hasntWorked.length > 0 &&
+      setInterestingData(createGraphData(hasWorked, hasntWorked));
+  }, [createGraphData, hasWorked, hasntWorked]);
 
-    const data = {
-      labels: painkillerCorrelation.map(
-        (dataPoint: { painkiller_name: string }) => dataPoint.painkiller_name
-      ),
-      datasets: datasets,
-    };
-    return data;
-  }
+  // function createDiseaseArray(
+  //   painkillerCorrelation: PainkillerCorrelationType[]
+  // ) {
+  //   const diseaseArray: number[] = [];
+  //   for (const obj of painkillerCorrelation) {
+  //     for (const [key, value] of Object.entries(obj)) {
+  //       if (key === "condition_id" && !diseaseArray.includes(value)) {
+  //         diseaseArray.push(value);
+  //       }
+  //     }
+  //   }
+  //   return diseaseArray;
+  // }
+
+  // function createChartData(painkillerCorrelation: PainkillerCorrelationType[]) {
+  //   const datasets = [];
+
+  //   for (const disease of createDiseaseArray(painkillerCorrelation)) {
+  //     const oneLine = {
+  //       label: findConditionName(painkillerCorrelation, disease),
+  //       data: filterDataByCondition(painkillerCorrelation, disease).map(
+  //         (dataPoint: { avg: string }) => parseInt(dataPoint.avg)
+  //       ),
+  //       backgroundColor: "#FF0080",
+  //       borderColor: "#FF8C00",
+  //     };
+  //     datasets.push(oneLine);
+  //   }
+
+  //   const data = {
+  //     labels: painkillerCorrelation.map(
+  //       (dataPoint: { painkiller_name: string }) => dataPoint.painkiller_name
+  //     ),
+  //     datasets: datasets,
+  //   };
+  //   return data;
+  // }
 
   return (
     <>
@@ -88,27 +153,28 @@ export default function InterestingData(): JSX.Element {
           ⚠️Caution is advised when reading this data, correlation does not
           infer causation⚠️
         </small>
+
         <hr />
-        <Bar className="chart" data={createChartData(painkillerCorrelation)} />
+        {/* <Bar className="chart" data={createChartData(painkillerCorrelation)} /> */}
       </div>
     </>
   );
 }
 
-function findConditionName(
-  painkillerCorrelation: PainkillerCorrelationType[],
-  element: number
-) {
-  return painkillerCorrelation.find(
-    (condition) => condition.condition_id === element
-  )?.condition_name;
-}
+// function findConditionName(
+//   painkillerCorrelation: PainkillerCorrelationType[],
+//   element: number
+// ) {
+//   return painkillerCorrelation.find(
+//     (condition) => condition.condition_id === element
+//   )?.condition_name;
+// }
 
-function filterDataByCondition(
-  painkillerCorrelation: PainkillerCorrelationType[],
-  disease: number
-) {
-  return painkillerCorrelation.filter(
-    (condition) => condition.condition_id === disease
-  );
-}
+// function filterDataByCondition(
+//   painkillerCorrelation: PainkillerCorrelationType[],
+//   disease: number
+// ) {
+//   return painkillerCorrelation.filter(
+//     (condition) => condition.condition_id === disease
+//   );
+// }
---
+++ src/utils/Types/hasWorkedType.ts
@@ -0,0 +1,5 @@
+export interface hasWorkedType {
+  painkiller_name: string;
+  condition_name: string;
+  has_worked: string;
+}
